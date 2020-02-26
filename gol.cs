999366de7bf2b5c377e2bb8ed417578610dc1c5f
cesilko1 <wilhelm.master@gmail.com>
2020-02-26 11:44:23 +0100

    World generating

--- gol.cs
+++ gol.cs
@@ -1,9 +1,46 @@
 using System;
+using Cells;
 
 class gol {
 
 	static void Main(string[] args) {
 		
+		//disable cursor
+		Console.CursorVisible = false;
+		Console.Clear();
+
+		//get width and height of world
+		int WorldWidth = Console.WindowWidth;
+		int WorldHeight = Console.WindowHeight;
+
+		//create an array of world matrix
+		Cell[,] World = new Cell[WorldWidth, WorldHeight];
+
+
+		for(int Y = 0; Y < WorldHeight; Y++) {
+
+			for(int X = 0; X < WorldWidth; X++) {
+
+				World[X, Y] = new Cell(X, Y, '#', '.');
+
+			}
+
+		}
+
+		
+		for(int Y = 0; Y < WorldHeight; Y++) {
+
+			for(int X = 0; X < WorldWidth; X++) {
+
+				Console.Write(World[X, Y].Show());
+
+			}
+
+			Console.WriteLine();
+
+		}
+
+		Console.ReadKey();
 	}
-	
+
 }
--- lib/cell.cs
+++ lib/cell.cs
@@ -1,24 +1,42 @@
 using System;
 
-namespace cells {
+namespace Cells {
 
 	public class Cell {
 
-		//status of cell
-		private bool Alive = false;
+		//status of cell with public getter and private setter
+		public bool Alive { get; private set; } = false;
 
 		//position of cell
-		private int X_pos;
-		private int Y_pos;
+		public int PositionX { get; private set; }
+		public int PositionY { get; private set; }
 
-		//chars that show in matrix
-		private string AliveChar;
-		private string DeadChar;
+		//characters to represent cell in console
+		public char AliveChar { get; private set; } = '#';
+		public char DeadChar { get; private set; } = ' ';
 
 
-		//constructor for cell
-		public Cell() {
+		//constructor for cell with position
+		public Cell(int PositionX, int PositionY) {
+			this.PositionX = PositionX;
+			this.PositionY = PositionY;
+		}
 
+		//constructor for cell with position and char definitions
+		public Cell(int PositionX, int PositionY, char AliveChar, char DeadChar) {
+			this.PositionX = PositionX;
+			this.PositionY = PositionY;
+			this.AliveChar = AliveChar;
+			this.DeadChar  = DeadChar;
+		}
+
+		public char Show() {
+			if(Alive) {
+				return AliveChar;
+			}
+			else {
+				return DeadChar;
+			}
 		}
 	}
 
