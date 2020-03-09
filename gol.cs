c3f24fabfe520a351f396310bf948a55afc2d3ea
cesilko1 <wilhelm.master@gmail.com>
2020-03-09 20:43:56 +0100

    menu files update

--- gol.cs
+++ gol.cs
@@ -1,6 +1,7 @@
 using System;
 using GolWorld;
 using System.Threading;
+using Menu;
 
 class gol {
 
---
+++ lib/consoleMenu.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace Menu {
+
+	public class ConsoleMenu {
+
+		private List<object> menuItems = new List<object>();
+
+		private string MenuHeader;
+
+ 
+
+
+		public ConsoleMenu(string MenuHeader) {
+			this.MenuHeader = MenuHeader;
+		}
+
+	}
+
+}
