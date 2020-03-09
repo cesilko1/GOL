395a484c23b2abe1dde800150c3761fc492436a4
cesilko1 <wilhelm.master@gmail.com>
2020-03-09 21:01:35 +0100

    menu creating

--- gol.cs
+++ gol.cs
@@ -1,37 +1,14 @@
 using System;
 using GolWorld;
 using System.Threading;
+using System.Linq;
+using System.Text;
 using Menu;
 
 class gol {
 
 	static void Main(string[] args) {
-		int delay = 100;
 		
-		Console.CursorVisible = false;
-		
-		World world = new World(Console.WindowWidth-2, Console.WindowHeight-3);
-
-		world.OpenCursor();
-
-		Thread.Sleep(delay);
-
-		
-		for(int i = 0; i < 100; i++) {
-
-			world.NextGen();
-
-			world.Display();
-
-			Console.WriteLine();
-			int genNum = i+1;
-			Console.Write("generation: " + genNum);
-
-			Thread.Sleep(delay);
-		}
-		Console.WriteLine();
-
-		Console.CursorVisible = true;
 	}
 
 }
--- lib/consoleMenu.cs
+++ lib/consoleMenu.cs
@@ -6,14 +6,20 @@ namespace Menu {
 	public class ConsoleMenu {
 
 		private List<object> menuItems = new List<object>();
-
 		private string MenuHeader;
 
  
+		public ConsoleColor TextColor;
+		public ConsoleColor HighlightColor;
 
 
 		public ConsoleMenu(string MenuHeader) {
 			this.MenuHeader = MenuHeader;
+		}
+
+
+		public void AddItem(object Item) {
+			
 		}
 
 	}
