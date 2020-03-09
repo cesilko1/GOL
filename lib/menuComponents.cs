6aa0a5215fd69c4b9d3f00e6f798733cc6566030
cesilko1 <wilhelm.master@gmail.com>
2020-03-09 22:29:55 +0100

    consoleMenu

--- lib/menuComponents.cs
+++ lib/menuComponents.cs
@@ -2,6 +2,15 @@ using System;
 
 namespace Menu {
 
-	
+	public class Button {
+
+		private string ButtonName;
+
+		public Button(string ButtonName) {
+			this.ButtonName = ButtonName;
+		}
+
+
+	}
 
 }
